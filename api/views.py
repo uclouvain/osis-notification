dc520e493270ec99584a8e2c08182b5124f10b2d
jmr <jmr@makina-corpus.com>
2021-06-02 16:55:02 +0200

    Add a pagination class on the SentNotificationListView limiting results to 15

--- api/views.py
+++ api/views.py
@@ -1,8 +1,13 @@
 from rest_framework import generics
+from rest_framework.pagination import LimitOffsetPagination
 
 from osis_notification.api.permissions import IsNotificationRecipient
 from osis_notification.api.serializers import WebNotificationSerializer
 from osis_notification.models import WebNotification
+
+
+class NotificationSetPagination(LimitOffsetPagination):
+    default_limit = 15
 
 
 class SentNotificationListView(generics.ListAPIView):
@@ -11,6 +16,7 @@ class SentNotificationListView(generics.ListAPIView):
     queryset = WebNotification.objects.sent()
     serializer_class = WebNotificationSerializer
     permission_classes = (IsNotificationRecipient,)
+    pagination_class = NotificationSetPagination
 
     def get_queryset(self):
         return super().get_queryset().filter(person__uuid=self.kwargs["uuid"])
