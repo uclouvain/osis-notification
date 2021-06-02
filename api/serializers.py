dae709a0896389e48a16b51c3f9018bada441a9d
jmr <jmr@makina-corpus.com>
2021-06-02 15:07:05 +0200

    Add 'pk' and 'read_at' on web notification serializer

--- api/serializers.py
+++ api/serializers.py
@@ -7,8 +7,10 @@ class WebNotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = WebNotification
         fields = [
+            "pk",
             "state",
             "payload",
             "created_at",
             "sent_at",
+            "read_at",
         ]
