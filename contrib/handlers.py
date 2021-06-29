e75645cda0dcc9fd11deebb542a8a6023e2124bf
jmr <jmr@makina-corpus.com>
2021-06-29 11:38:42 +0200

    Rebuilding base python object that created db objects to call pre_process and post_process methods

--- contrib/handlers.py
+++ contrib/handlers.py
@@ -41,21 +41,45 @@ class EmailNotificationHandler:
 
     @staticmethod
     def create(
-        mail: EmailMessage,
-        person: Optional[Person] = None,
+        mail: Optional[EmailMessage] = None,
+        notification: Optional[EmailNotificationType] = None,
     ) -> EmailNotification:
-        """Create a email notification from a python object and save it in the database.
+        """Create a email notification either from a python object or from an
+        EmailMessage and save it in the database.
 
         :param mail: The email message to be send as a notification.
-        :param person: The recipient of the notification.
+        :param notification: A python object representing the email notification.
         :return: The created EmailNotification."""
 
-        if person is None:
-            person = Person.objects.get(email=mail["To"])
+        if mail is None and notification is None:
+            raise ValueError(
+                "Please either give an email message or a EmailNotification"
+            )
+        elif mail is None:
+            # build the mail from python object
+            mail = EmailNotificationHandler.build(notification)
+            person = notification.recipient
+        else:
+            person = Person.objects.get(user__email=mail["To"])
+            plain_text_content = ""
+            html_content = ""
+            for part in mail.walk():
+                if part.get_content_type() == "text/plain":
+                    plain_text_content = part.get_payload()
+                elif part.get_content_type() == "text/html":
+                    html_content = part.get_payload()
+            notification = EmailNotificationType(
+                recipient=person,
+                subject=mail['Subject'],
+                plain_text_content=plain_text_content,
+                html_content=html_content,
+            )
 
         return EmailNotification.objects.create(
             person=person,
             payload=mail.as_string(),
+            built_from_module=notification.__module__,
+            built_from_class_name=notification.__class__.__name__,
         )
 
     @staticmethod
@@ -70,8 +94,10 @@ class EmailNotificationHandler:
             notification.person.email,
             settings.LANGUAGE_CODE,
         )
-        plain_text_content = ''
-        html_content = ''
+        plain_text_content = ""
+        html_content = ""
+        subject = unescape(strip_tags(email_message.get("subject")))
+
         for part in email_message.walk():
             if part.get_content_type() == "text/plain":
                 plain_text_content = part.get_payload()
@@ -84,7 +110,7 @@ class EmailNotificationHandler:
                 receivers=[receiver],
                 reference=None,
                 connected_user=None,
-                subject=unescape(strip_tags(email_message.get("subject"))),
+                subject=subject,
                 message=plain_text_content,
                 html_message=html_content,
                 from_email=settings.DEFAULT_FROM_EMAIL,
@@ -94,7 +120,24 @@ class EmailNotificationHandler:
             mail_sender.send_mail()
         notification.state = NotificationStates.SENT_STATE.name
         notification.sent_at = now()
+
+        # rebuilt the base notification Python object
+        base_class = import_string(
+            f"{notification.built_from_module}.{notification.built_from_class_name}"
+        )
+        # Now rebuilt the python object from the class we have
+        #   given the notification object we have from the DB
+        base_object = base_class(
+            recipient=notification.person,
+            subject=subject,
+            plain_text_content=plain_text_content,
+            html_content=html_content,
+        )
+        # Then call the pre_process method
+        base_object.pre_process()
         notification.save()
+        # Then call the post_process method
+        base_object.post_process()
 
 
 class WebNotificationHandler:
@@ -108,6 +151,8 @@ class WebNotificationHandler:
         return WebNotification.objects.create(
             person=notification.recipient,
             payload=notification.content,
+            built_from_module=notification.__module__,
+            built_from_class_name=notification.__class__.__name__,
         )
 
     @staticmethod
@@ -116,7 +161,21 @@ class WebNotificationHandler:
 
         notification.state = NotificationStates.SENT_STATE.name
         notification.sent_at = now()
+
+        # rebuilt the base notification Python object
+        base_class = import_string(
+            f"{notification.built_from_module}.{notification.built_from_class_name}"
+        )
+        # Now rebuilt the python object from the class we have
+        #   given the notification object we have from the DB
+        base_object = base_class(
+            recipient=notification.person, content=notification.payload
+        )
+        # Then call the pre_process method
+        base_object.pre_process()
         notification.save()
+        # Then call the post_process method
+        base_object.post_process()
 
     @staticmethod
     def toggle_state(notification: WebNotification):
