0f31d350a1616b5465dee14e4de4b58b50c60c75
Sebastien Corbin <seb.corbin@gmail.com>
2021-07-05 11:49:03 +0200

    Format dates in web notifications

--- api/serializers.py
+++ api/serializers.py
@@ -4,6 +4,10 @@ from osis_notification.models import WebNotification
 
 
 class WebNotificationSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format='%d/%m/%Y %H:%M')
+    sent_at = serializers.DateTimeField(format='%d/%m/%Y %H:%M')
+    read_at = serializers.DateTimeField(format='%d/%m/%Y %H:%M')
+
     class Meta:
         model = WebNotification
         fields = [
