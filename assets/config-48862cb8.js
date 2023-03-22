160e2a4bc0ba20b7001a1afa51c7e972a5b4bac9
albrugnetti <alessandro.brugnetti@uclouvain.be>
2023-03-22 15:29:47 +0000

    Deploying to gh-pages from  @ 6f74711751cfe17a3a8610a5754091730c56ca26 🚀

---
+++ assets/config-48862cb8.js
@@ -0,0 +1,2 @@
+import{d as t,r as m,a as s}from"./decorateStory-126df8e4.js";import"./iframe-48cdc47f.js";import"./vue.esm-bundler-f87cfd15.js";var a={framework:"vue3"};export{t as applyDecorators,a as parameters,m as render,s as renderToDOM};
+//# sourceMappingURL=config-48862cb8.js.map
