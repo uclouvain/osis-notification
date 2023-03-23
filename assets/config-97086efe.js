056c80786ea30dcb68ec7e25616bd23a88f5b57e
edd19 <eddyndizera@gmail.com>
2023-03-23 09:22:58 +0000

    Deploying to gh-pages from  @ 1b202c8e157580af1662a4ea4801b4afcb03298f 🚀

---
+++ assets/config-97086efe.js
@@ -0,0 +1,2 @@
+import{d as t,r as m,a as s}from"./decorateStory-0c659626.js";import"./iframe-596a554c.js";import"./vue.esm-bundler-f87cfd15.js";var a={framework:"vue3"};export{t as applyDecorators,a as parameters,m as render,s as renderToDOM};
+//# sourceMappingURL=config-97086efe.js.map
