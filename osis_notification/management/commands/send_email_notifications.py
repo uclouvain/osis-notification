24a7a26ad617065873687bb60dbbf7b6d61454c5
Julien Cougnaud <julien.cougnaud@makina-corpus.com>
2023-12-08 12:18:17 +0100

    [OSIS-8748] Better manage the errors when sending the emails

--- osis_notification/management/commands/send_email_notifications.py
+++ osis_notification/management/commands/send_email_notifications.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 
 from osis_notification.contrib.handlers import EmailNotificationHandler
+from osis_notification.contrib.exceptions import EmailNotificationSendingException
 from osis_notification.models import EmailNotification
 
 
@@ -8,5 +9,13 @@ class Command(BaseCommand):
     help = "Send all the email notifications."
 
     def handle(self, *args, **options):
+        exceptions = {}
+
         for notification in EmailNotification.objects.pending():
-            EmailNotificationHandler.process(notification)
+            try:
+                EmailNotificationHandler.process(notification)
+            except Exception as exception:
+                exceptions[notification.uuid] = exception
+
+        if exceptions:
+            raise EmailNotificationSendingException(exceptions)
--- osis_notification/tests/test_commands.py
+++ osis_notification/tests/test_commands.py
@@ -1,8 +1,9 @@
 from datetime import timedelta
+from unittest.mock import patch
 
 from django.conf import settings
 from django.core.management import call_command
-from django.test import override_settings, TestCase
+from django.test import override_settings
 from django.utils.timezone import now
 
 from base.tests.factories.person import PersonFactory
@@ -10,7 +11,8 @@ from osis_notification.contrib.handlers import EmailNotificationHandler
 from osis_notification.contrib.notification import (
     EmailNotification as EmailNotificationType,
 )
-from osis_notification.models import EmailNotification, WebNotification
+from osis_notification.contrib.exceptions import EmailNotificationSendingException
+from osis_notification.models import EmailNotification, WebNotification, Notification
 from osis_notification.models.enums import NotificationStates
 from osis_notification.tests import TestCase
 from osis_notification.tests.factories import (
@@ -18,11 +20,12 @@ from osis_notification.tests.factories import (
     WebNotificationFactory,
 )
 
+from osis_common.messaging.mail_sender_classes import MessageHistorySender
+
 
 class SendNotificationsTest(TestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.web_notification = WebNotificationFactory()
+    def setUp(self):
+        self.web_notification = WebNotificationFactory()
         # It's seems a bit more complicated to create the email message payload in a
         # factory so I leave it like this for the moment
         email_notification_data = {
@@ -33,7 +36,7 @@ class SendNotificationsTest(TestCase):
         }
         email_notification = EmailNotificationType(**email_notification_data)
         email_message = EmailNotificationHandler.build(email_notification)
-        cls.email_notification = EmailNotificationFactory(
+        self.email_notification = EmailNotificationFactory(
             payload=email_message.as_string(),
             person=email_notification_data["recipient"],
         )
@@ -41,27 +44,63 @@ class SendNotificationsTest(TestCase):
     def test_send_email_notifications(self):
         # ensure email notification is in pending state after creation
         self.assertEqual(
-            self.email_notification.state, NotificationStates.PENDING_STATE.name
+            self.email_notification.state,
+            NotificationStates.PENDING_STATE.name,
         )
         with self.assertNumQueriesLessThan(6):
             call_command("send_email_notifications")
         self.email_notification.refresh_from_db()
         # now email notification should be in sent state
         self.assertEqual(
-            self.email_notification.state, NotificationStates.SENT_STATE.name
+            self.email_notification.state,
+            NotificationStates.SENT_STATE.name,
         )
+
+    def test_send_email_notifications_with_failure(self):
+        second_email_notification = EmailNotificationFactory(
+            payload=self.email_notification.payload,
+            person=self.email_notification.person,
+        )
+        third_email_notification = EmailNotificationFactory(
+            payload=self.email_notification.payload,
+            person=self.email_notification.person,
+        )
+
+        original_send_mail = Notification.save
+        with patch.object(Notification, 'save') as sender_mock:
+            sender_mock.side_effect = [original_send_mail, ValueError('Invalid value'), Exception('Custom error')]
+
+            with self.assertRaisesRegex(
+                EmailNotificationSendingException,
+                f'Errors occurred while sending the email notifications:'
+                f'\n{second_email_notification.uuid}: Invalid value.'
+                f'\n{third_email_notification.uuid}: Custom error.'
+            ):
+                call_command("send_email_notifications")
+
+                self.email_notification.refresh_from_db()
+                self.assertEqual(self.email_notification.state, NotificationStates.SENT_STATE.name)
+
+                second_email_notification.refresh_from_db()
+                self.assertEqual(second_email_notification.state, NotificationStates.PENDING_STATE.name)
+
+                third_email_notification.refresh_from_db()
+                self.assertEqual(second_email_notification.state, NotificationStates.PENDING_STATE.name)
+
 
     def test_send_web_notifications(self):
         # ensure web notification is in pending state after creation
         self.assertEqual(
-            self.web_notification.state, NotificationStates.PENDING_STATE.name
+            self.web_notification.state,
+            NotificationStates.PENDING_STATE.name,
         )
         with self.assertNumQueriesLessThan(3):
             call_command("send_web_notifications")
         self.web_notification.refresh_from_db()
         # now web notification should be in sent state
         self.assertEqual(
-            self.web_notification.state, NotificationStates.SENT_STATE.name
+            self.web_notification.state,
+            NotificationStates.SENT_STATE.name,
         )
 
 
